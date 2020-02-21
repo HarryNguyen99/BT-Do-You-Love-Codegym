c8fcf4648c7a959d54949fb0e3bee05e52053821
Harry Nguyen <phuquyit@gmail.com>
2020-02-22 02:06:25 +0700

    v1

---
+++ js/my.js
@@ -0,0 +1,4 @@
+function setRandomPosition() {
+    document.getElementById("image2").style.top = Math.floor(Math.random() * 100) +"px";
+    document.getElementById("image2").style.right = Math.floor(Math.random() * 700) +"px";
+}
